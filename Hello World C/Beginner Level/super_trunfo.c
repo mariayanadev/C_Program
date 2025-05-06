e4323e0cc2556325cc6843020129b0f1bce252fb
mariayanadev <yana.tech06@gmail.com>
2025-05-05 22:33:03 -0300

    Printing, storing and manipulating input and output data

---
+++ Hello World C/Beginner Level/super_trunfo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    char State;
+    //codeCharter, City;
+    //int Population, n_TouristAttractions;
+    //float Area, PIB;
+
+    printf("Em qual Estado você habita?\n");
+    scanf(" %c",&State);
+    printf("Nome do seu Estado é %c\n",State);
+
+    return 0;
+}
